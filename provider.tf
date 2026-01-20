b43faccdaa4742b8296818ccd13c15dc76c898a5
MdMusfikurRahmanSifar <121184191+MdMusfikurRahmanSifar@users.noreply.github.com>
2026-01-20 20:32:03 +0600

    Configure S3 backend for Terraform state

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
-terraform {}
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-123sifar"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 
 provider "aws" {
   region = "ap-south-1"
